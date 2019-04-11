758822f22d0e1f2d742bb30c53ba49dd0f4ceae5
ilabstaff <ilabstaff@172.16.116.77>
2019-04-10 17:57:40 -0700

    animal farm

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -15,11 +15,12 @@ public class AnimalFarm {
 	AnimalFarm() {
 		/*
 		 * 1. Ask the user which animal they want, then play the sound of that animal.
-		 */
-		
+		 */String animal=JOptionPane.showInputDialog("what animal do you like");
+		if(animal.equals("cow")) {
+			playMoo();
 		/* 2. Make it so that the user can keep entering new animals. */
 	}
-
+	}
 	void playMoo() {
 		playNoise(mooFile);
 	}
